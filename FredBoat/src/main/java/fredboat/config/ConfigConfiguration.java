ad22c9f1dc65ed8e864be81d055f2bc1190064ee
Napster <napster@npstr.space>
2018-02-23 22:48:28 +0100

    Add refreshing beans to access each property config individually

---
+++ FredBoat/src/main/java/fredboat/config/ConfigConfiguration.java
@@ -0,0 +1,77 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2018 Frederik Ar. Mikkelsen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package fredboat.config;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Created by napster on 23.02.18.
+ * <p>
+ * The name is not a meme, I swear.
+ * <p>
+ * Provide refreshing beans to access property configs
+ */
+@Configuration
+public class ConfigConfiguration {
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public AppConfig appConfig(PropertyConfigProvider configProvider) {
+        return configProvider.getAppConfig();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public AudioSourcesConfig audioSourcesConfig(PropertyConfigProvider configProvider) {
+        return configProvider.getAudioSourcesConfig();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public Credentials credentials(PropertyConfigProvider configProvider) {
+        return configProvider.getCredentials();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public DatabaseConfig databaseConfig(PropertyConfigProvider configProvider) {
+        return configProvider.getDatabaseConfig();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public EventLoggerConfig eventLoggerConfig(PropertyConfigProvider configProvider) {
+        return configProvider.getEventLoggerConfig();
+    }
+
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public LavalinkConfig lavalinkConfig(PropertyConfigProvider configProvider) {
+        return configProvider.getLavalinkConfig();
+    }
+}
