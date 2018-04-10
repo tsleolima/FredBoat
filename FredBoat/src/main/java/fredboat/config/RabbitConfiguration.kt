14ef9a21c4f340e632a58f7c7ef884ce5ca4f4ff
Frederikam <frogkr@gmail.com>
2018-04-10 12:25:22 +0200

    Create a reactive sentinel client

--- FredBoat/src/main/java/fredboat/config/RabbitConfiguration.kt
+++ FredBoat/src/main/java/fredboat/config/RabbitConfiguration.kt
@@ -6,6 +6,10 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+
+
 
 @Configuration
 open class RabbitConfiguration {
@@ -15,5 +19,11 @@ open class RabbitConfiguration {
         // We must register this Kotlin module to get deserialization to work with data classes
         return Jackson2JsonMessageConverter(ObjectMapper().registerKotlinModule())
     }
+
+    @Bean
+    open fun asyncTemplate(rabbitTemplate: RabbitTemplate): AsyncRabbitTemplate {
+        return AsyncRabbitTemplate(rabbitTemplate)
+    }
+
 
 }
---
+++ FredBoat/src/main/java/fredboat/rabbit/Sentinel.kt
@@ -0,0 +1,36 @@
+package fredboat.rabbit
+
+import com.fredboat.sentinel.entities.*
+import org.springframework.amqp.rabbit.AsyncRabbitTemplate
+import org.springframework.stereotype.Component
+import org.springframework.util.concurrent.SuccessCallback
+import reactor.core.publisher.Mono
+
+@Component
+class Sentinel(private val template: AsyncRabbitTemplate){
+
+    fun sendMessage(channel: TextChannel, message: String): Mono<SendMessageResponse> = Mono.create {
+        val req = SendMessageRequest(channel.id, message)
+        template.convertSendAndReceive<SendMessageResponse>(req).addCallback(
+                {res -> it.success(res)},
+                {exc -> it.error(exc)}
+        )
+    }
+
+    fun sendTyping(channel: TextChannel): Mono<Unit> = Mono.create {
+        val req = SendTypingRequest(channel.id)
+        template.convertSendAndReceive<Unit>(req).addCallback(
+                {_   -> it.success()},
+                {exc -> it.error(exc)}
+        )
+    }
+
+    fun getGuilds(shard: Shard): Mono<List<Guild>> = Mono.create {
+        val req = GuildsRequest(shard.id)
+        template.convertSendAndReceive<GuildsResponse>(req).addCallback(
+                {res -> it.success(res?.guilds)},
+                {exc -> it.error(exc)}
+        )
+    }
+
+}
