902aaf63ba594600d5f5a35fb709560ab23bdedc
Frederik Mikkelsen <frogkr@gmail.com>
2018-04-14 23:37:03 +0200

    Fix sentinel and rabbit consumer

--- FredBoat/src/main/java/fredboat/rabbit/RabbitConsumer.kt
+++ FredBoat/src/main/java/fredboat/rabbit/RabbitConsumer.kt
@@ -1,11 +1,9 @@
-package fredboat.event
+package fredboat.rabbit
 
 import com.fredboat.sentinel.QueueNames
 import com.fredboat.sentinel.entities.*
 import fredboat.command.CommandContext
-import fredboat.rabbit.Member
-import fredboat.rabbit.Message
-import fredboat.rabbit.TextChannel
+import fredboat.command.TestCommand
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.annotation.RabbitHandler
@@ -15,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap
 
 @Service
 @RabbitListener(queues = [QueueNames.JDA_EVENTS_QUEUE])
-class RabbitConsumer {
+class RabbitConsumer(private val sentinel: Sentinel) {
 
     companion object {
         private val log: Logger = LoggerFactory.getLogger(RabbitConsumer::class.java)
@@ -46,25 +44,29 @@ class RabbitConsumer {
 
     /* Voice events */
 
+    @RabbitHandler
     fun receive(event: VoiceJoinEvent) {
         // TODO
     }
 
+    @RabbitHandler
     fun receive(event: VoiceLeaveEvent) {
         // TODO
     }
 
+    @RabbitHandler
     fun receive(event: VoiceMoveEvent) {
         // TODO
     }
 
     /* Message events */
 
+    @RabbitHandler
     fun receive(event: MessageReceivedEvent) {
         if(!event.content.startsWith(";;sentinel")) return
 
         val ctx = CommandContext(
-                fredboat.rabbit.Guild(event.guildId),
+                Guild(event.guildId),
                 Member(event.author),
                 TextChannel(event.channel),
                 Message(event)
@@ -73,8 +75,14 @@ class RabbitConsumer {
         TestCommand().invoke(ctx)
     }
 
+    @RabbitHandler
     fun receive(event: PrivateMessageReceivedEvent) {
         // TODO
+    }
+
+    @RabbitListener
+    fun guildInvalidate(event: GuildInvalidation) {
+        sentinel.guildCache.invalidate(event.id)
     }
 
     @RabbitHandler(isDefault = true)
