97ffac229a76266e87053f7ab7f18176c5a0a5b4
Frederik Mikkelsen <frogkr@gmail.com>
2018-04-29 19:38:03 +0200

    Properly use Flux

--- FredBoat/src/main/java/fredboat/rabbit/Sentinel.kt
+++ FredBoat/src/main/java/fredboat/rabbit/Sentinel.kt
@@ -12,6 +12,7 @@ import org.springframework.amqp.rabbit.AsyncRabbitTemplate
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.util.concurrent.TimeUnit
 
@@ -46,13 +47,16 @@ class Sentinel(private val template: AsyncRabbitTemplate,
                     })
             )
 
-    fun getGuilds(shard: Shard): Mono<List<RawGuild>> = Mono.create {
+    fun getGuilds(shard: Shard): Flux<RawGuild> = Flux.create {
         val req = GuildsRequest(shard.id)
         template.convertSendAndReceive<GuildsResponse?>(req).addCallback(
                 { res ->
-                    it.success(res?.guilds)
-                    // We might not actually want to cache these
-                    //res?.guilds?.forEach { guildCache.put(it.id, it) }
+                    if (res != null) {
+                        res.guilds.forEach { t -> it.next(t) }
+                        it.complete()
+                    } else {
+                        it.error(RuntimeException("Response was null"))
+                    }
                 },
                 { exc -> it.error(exc) }
         )
